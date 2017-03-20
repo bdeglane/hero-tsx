a8b0aaffd176bf766cc8c6a529f73a19e14e7059
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-03-20 23:27:44 +0100

    create heroes reducer

---
+++ src/app/reducer/heroes.ts
@@ -0,0 +1,32 @@
+import {
+    GET_HERO,
+    GET_HEROES,
+    SET_HEROES
+} from '../constant/heroes';
+import {HEROES} from "./helper/mock-heroes";
+import Hero from "../model/Hero";
+import {Action} from "../action/action.i";
+
+interface heroesState {
+
+}
+
+const defaultState = (): Hero[] => {
+    return [];
+};
+
+export const heroes = (state = defaultState(), action: any): any => {
+
+    switch (action.type) {
+        case GET_HEROES:
+            break;
+        case SET_HEROES:
+            state = [...state, ...action.payload.heroes];
+            break;
+        case GET_HERO:
+            break;
+        default:
+            break;
+    }
+    return state;
+};
