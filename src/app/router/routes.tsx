ad9b7c070d8af82004b410f986e783cb79db54e7
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-03-20 23:28:16 +0100

    add dashboard page

--- src/app/router/routes.tsx
+++ src/app/router/routes.tsx
@@ -5,9 +5,12 @@ import {
 } from 'react-router';
 
 import {Hello} from '../component/Hello';
+import DashboardPage from "../page/dashboardPage/index";
 
 export const routes = (
     <Route path="/">
         <IndexRoute component={Hello}/>
+        <Route path="dashboard"
+               component={DashboardPage}/>
     </Route>
 );
