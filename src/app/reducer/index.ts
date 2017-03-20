8730c1837c9a7c9fe9fa6bd980f0167fbc6638e6
Benoit Deglane <benoit.deglane@netapsys.fr>
2017-03-20 23:28:01 +0100

    register heroes reducer

--- src/app/reducer/index.ts
+++ src/app/reducer/index.ts
@@ -1,6 +1,9 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
+import {heroes} from './heroes';
+
 export const reducers = combineReducers({
-    routing: routerReducer
+    routing: routerReducer,
+    heroes
 });
